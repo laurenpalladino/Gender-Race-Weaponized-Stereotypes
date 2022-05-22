1d06327166af0530e5e1049c15a810a10fba0ded
laurenpalladino <73915362+laurenpalladino@users.noreply.github.com>
2022-05-22 12:14:25 -0400

    Update HHH undergrad sample.R

--- HHH undergrad sample.R
+++ HHH undergrad sample.R
@@ -2,7 +2,8 @@ library(readr)
 library(dplyr)
 library(survey)
 library(ggplot2)
-lauren600 <- read_csv("Downloads/Nicole+&+Lauren's+POL+600-Undergrad_March+7,+2022_07.43.csv")
+library(stargazer)
+lauren600 <- read_csv("Documents/POL 600/Undergrad data.csv")
 head(lauren600) 
 names(lauren600)
 
@@ -25,7 +26,7 @@ table(lauren600$candidate_race)
 table(lauren600$attack, lauren600$candidate_race)
 
 #competent
-lauren600$dv1 <- recode(lauren600$Q2.8, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
+lauren600$dv1 <- dplyr::recode(lauren600$Q2.8, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
 table(lauren600$dv1)
 
 m1 <- lm(dv1 ~ -1 +attack, data=lauren600)
@@ -34,7 +35,7 @@ summary(m1)
 m2 <- lm(dv1 ~ -1 +candidate_race, data=lauren600)
 summary(m2) 
 
-m3 <- lm(dv1 ~ -1 + attack*candidate_race, data=lauren600)
+m3 <- lm(dv1 ~ attack*candidate_race, data=lauren600)
 summary(m3) 
 
 lauren600_survey <- svydesign(ids = ~1, data=lauren600)
@@ -51,10 +52,10 @@ ggplot(m3_survey, aes(x=candidate_race, y=dv1)) +
   scale_y_continuous(name="Perceived competence", limits=c(0,1)) + 
   scale_x_discrete(name="Candidate racial cue", labels=c("White cue", "Black cue")) + 
   theme_bw() + displayprefs
-dev.off()
+
 
 #honesty
-lauren600$dv2 <- recode(lauren600$Q2.9, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
+lauren600$dv2 <- dplyr::recode(lauren600$Q2.9, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
 table(lauren600$dv2)
 
 m4 <- lm(dv2 ~ -1 +attack, data=lauren600)
@@ -63,7 +64,7 @@ summary(m4)
 m5 <- lm(dv2 ~  -1 + candidate_race, data=lauren600)
 summary(m5) 
 
-m6 <- lm(dv2 ~ -1 + attack*candidate_race, data=lauren600)
+m6 <- lm(dv2 ~ attack*candidate_race, data=lauren600)
 summary(m6) 
 
 lauren600_survey <- svydesign(ids = ~1, data=lauren600)
@@ -80,10 +81,9 @@ ggplot(honest_survey, aes(x=candidate_race, y=dv2)) +
   scale_y_continuous(name="Perceived honesty", limits=c(0,1)) + 
   scale_x_discrete(name="Candidate racial cue", labels=c("White cue", "Black cue")) + 
   theme_bw() + displayprefs
-dev.off()
 
 #concern
-lauren600$dv3 <- recode(lauren600$Q2.10, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
+lauren600$dv3 <- dplyr::recode(lauren600$Q2.10, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
 table(lauren600$dv3)
 
 m7 <- lm(dv3 ~ -1 + attack, data=lauren600)
@@ -92,7 +92,7 @@ summary(m7)
 m8 <- lm(dv3 ~ -1 + candidate_race, data=lauren600)
 summary(m8) 
 
-m9 <- lm(dv3 ~ -1 + attack*candidate_race, data=lauren600)
+m9 <- lm(dv3 ~ attack*candidate_race, data=lauren600)
 summary(m9) 
 
 lauren600_survey <- svydesign(ids = ~1, data=lauren600)
@@ -109,10 +109,9 @@ ggplot(concern_survey, aes(x=candidate_race, y=dv3)) +
   scale_y_continuous(name="Perceived concern", limits=c(0,1)) + 
   scale_x_discrete(name="Candidate racial cue", labels=c("White cue", "Black cue")) + 
   theme_bw() + displayprefs
-dev.off()
 
 #reliable
-lauren600$dv4 <- recode(lauren600$Q2.11, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
+lauren600$dv4 <- dplyr::recode(lauren600$Q2.11, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
 table(lauren600$dv4)
 
 m10 <- lm(dv4 ~ -1 + attack, data=lauren600)
@@ -121,7 +120,7 @@ summary(m10)
 m11 <- lm(dv4 ~  -1 + candidate_race, data=lauren600)
 summary(m11) 
 
-m12 <- lm(dv4 ~ -1 + attack*candidate_race, data=lauren600)
+m12 <- lm(dv4 ~ attack*candidate_race, data=lauren600)
 summary(m12) 
 
 lauren600_survey <- svydesign(ids = ~1, data=lauren600)
@@ -138,10 +137,9 @@ ggplot(reliable_survey, aes(x=candidate_race, y=dv4)) +
   scale_y_continuous(name="Perceived concern", limits=c(0,1)) + 
   scale_x_discrete(name="Candidate racial cue", labels=c("White cue", "Black cue")) + 
   theme_bw() + displayprefs
-dev.off()
 
 #same values
-lauren600$dv5 <- recode(lauren600$Q2.12, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
+lauren600$dv5 <- dplyr::recode(lauren600$Q2.12, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
 table(lauren600$dv5)
 
 m13 <- lm(dv5 ~ -1 + attack, data=lauren600)
@@ -150,7 +148,7 @@ summary(m13)
 m14 <- lm(dv5 ~ -1 + candidate_race, data=lauren600)
 summary(m14) 
 
-m15 <- lm(dv5 ~ -1 + attack*candidate_race, data=lauren600)
+m15 <- lm(dv5 ~ attack*candidate_race, data=lauren600)
 summary(m15) 
 
 lauren600_survey <- svydesign(ids = ~1, data=lauren600)
@@ -167,18 +165,46 @@ ggplot(values_survey, aes(x=candidate_race, y=dv5)) +
   scale_y_continuous(name="Share the same values", limits=c(0,1)) + 
   scale_x_discrete(name="Candidate racial cue", labels=c("White cue", "Black cue")) + 
   theme_bw() + displayprefs
-dev.off()
-
-#pretests
 
 hs<-(lauren600$`Hostile Sexism 1`+ lauren600$`Hostile Sexism 2` + lauren600$`Hostile Sexism 3`)
 table(hs)
 
-rr2<- recode(lauren600$`RR 2`, "1"=5, "2"=4, "3"=3, "4"=2, "5"=1 )
-rr3<- recode(lauren600$`RR 3`, "1"=5, "2"=4, "3"=3, "4"=2, "5"=1 )
+rr2<- dplyr::recode(lauren600$`RR 2`, "1"=5, "2"=4, "3"=3, "4"=2, "5"=1 )
+rr3<- dplyr::recode(lauren600$`RR 3`, "1"=5, "2"=4, "3"=3, "4"=2, "5"=1 )
 
 rr<-(lauren600$`RR 1` + rr2 + rr3 + lauren600$`RR 4`)
 table(rr)
 
 intmodel <- lm(dv5 ~ rr*hs, data=lauren600)
 summary(intmodel)
+
+#composite measure of trust
+trust600 <- (lauren600$dv1 + lauren600$dv2 + lauren600$dv3 + lauren600$dv4 + lauren600$dv5)
+table(trust600)
+
+trustlm600 <- lm(trust600~candidate_race*attack + hs+ rr, data=lauren600)
+summary(trustlm600)
+stargazer(m3, m6, m9, m12, m15)
+
+undergradinteract <- effect('candidate_race*attack', trustlm600, se=TRUE)
+undergraddf<-as.data.frame(undergradinteract)
+undergraddf$attack <- factor(undergraddf$attack,  
+                               level=c("control attack", "white attack", "black attack"),    
+                               labels=c("control attack", "white attack", "black attack"))   
+undergraddf$candidate_race <- factor(undergraddf$candidate_race,
+                             level=c("White cue", "Black cue"),   
+                             labels=c("White cue", "Black cue"))
+undergradplot<-ggplot(data=undergraddf, aes(x=attack, y=fit, group=candidate_race))+
+  geom_line(size=2, aes(color=candidate_race))+
+  geom_ribbon(aes(ymin=fit-se, ymax=fit+se,fill=candidate_race),alpha=.2)+
+  ylab("Trust")+
+  xlab("Attack Type")+
+  theme_bw()+
+  theme(text = element_text(size=12),
+        legend.text = element_text(size=12),
+        legend.direction = "horizontal",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position="top")
+undergradplot
+
