7f2ce425e85724eb5ed41fcd37d33a387c9bf5c8
laurenpalladino <73915362+laurenpalladino@users.noreply.github.com>
2025-06-05 17:46:44 -0400

    Update Study 2.R

--- Study 2.R
+++ Study 2.R
@@ -4,7 +4,8 @@ library(survey)
 library(stargazer)
 library(effects)
 library(ggplot2)
-mturk <- read_csv("Documents/[RESEARCH] HHH/Experiment 2/mturk.csv")
+library(sjPlot)
+mturk <- read_csv("mturk.csv")
 attach(mturk)
 
 mturk$pid7 <-NA
@@ -41,12 +42,22 @@ mturk$attack[mturk$condition=="BABN"] <- "Black feminine attack"
 mturk$attack[mturk$condition=="BAWN"] <- "Black feminine attack"
 table(mturk$attack)
 
+mturk$congruent<-as.factor(NA)
+levels(mturk$congruent) <- c("congruent", "incongruent")
+mturk$congruent[mturk$condition=="WABN"] <- "incongruent"
+mturk$congruent[mturk$condition=="WAWN"] <- "congruent"
+mturk$congruent[mturk$condition=="BABN"] <- "congruent"
+mturk$congruent[mturk$condition=="BAWN"] <- "incongruent"
+table(mturk$congruent)
+
 mturk$candidate_race <- as.factor(NA)
 levels(mturk$candidate_race) <- c("Black cue", "white cue")
 mturk$candidate_race[mturk$candidate_name=="Althea Johnson"] <- "Black cue"
 mturk$candidate_race[mturk$candidate_name=="Susan Johnson"] <- "white cue"
 table(mturk$candidate_race)
 table(mturk$attack, mturk$candidate_race)
+
+t.test()
 
 #hostile sexism
 HS1<-as.numeric(mturk$HS1)
@@ -65,61 +76,32 @@ summary(mturk$RR)
 
 #competent
 mturk$dv1 <-dplyr::recode(mturk$Q2.8, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
-
-competentlm <- lm(dv1~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
-summary(competentlm)
-
-#graph
-plot_model(competentlm, type = "pred", terms = c("attack", "candidate_race"),
-           axis.title = c("Attack Type", "Competence"),
-           legend.title = "Candidate Race", title = "Predicted Competence")
+competent1 <- lm(dv1~attack+candidate_race + HS + RR + pid7 + female, data=mturk)
+competent2 <- lm(dv1~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
 
 #honesty
 mturk$dv2 <- dplyr::recode(mturk$Q2.9, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
-
-honestlm <- lm(dv2~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
-summary(honestlm)
-
-#graph
-plot_model(honestlm, type = "pred", terms = c("attack", "candidate_race"),
-           axis.title = c("Attack Type", "Honesty"),
-           legend.title = "Candidate Race", title = "Predicted Honesty")
+honest1 <- lm(dv2~attack+candidate_race + HS + RR + pid7 + female, data=mturk)
+honest2 <- lm(dv2~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
 
 #concern
 mturk$dv3 <- dplyr::recode(mturk$Q2.10, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
-
-concernlm <- lm(dv3~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
-summary(concernlm)
-
-#graph
-plot_model(concernlm, type = "pred", terms = c("attack", "candidate_race"),
-           axis.title = c("Attack Type", "Concern for Constituents"),
-           legend.title = "Candidate Race", title = "Predicted Concern")
+concern1 <- lm(dv3~attack+candidate_race + HS + RR + pid7 + female, data=mturk)
+concern2 <- lm(dv3~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
 
 #reliable
 mturk$dv4 <- dplyr::recode(mturk$Q2.11, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
-
-reliablelm <- lm(dv4~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
-summary(reliablelm)
-
-#graph
-plot_model(reliablelm, type = "pred", terms = c("attack", "candidate_race"),
-           axis.title = c("Attack Type", "Reliability"),
-           legend.title = "Candidate Race", title = "Predicted Reliability")
+reliable1 <- lm(dv4~attack+candidate_race + HS + RR + pid7 + female, data=mturk)
+reliable2 <- lm(dv4~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
 
 #same values
 mturk$dv5 <- dplyr::recode(mturk$Q2.12, "1"=1, "2"=0.75, "3"=0.5, "4"=0.25, "5"=0)
-
-valueslm <- lm(dv5~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
-summary(valueslm)
-
-#graph
-plot_model(valueslm, type = "pred", terms = c("attack", "candidate_race"),
-           axis.title = c("Attack Type", "Shares the Same Values I Do"),
-           legend.title = "Candidate Race", title = "Predicted Values")
+values1 <- lm(dv5~attack+candidate_race + HS + RR + pid7 + female, data=mturk)
+values2 <- lm(dv5~attack*candidate_race + HS + RR + pid7 + female, data=mturk)
 
 #table comparing the 5 whole-sample models
-stargazer(model2, model4, model6, model8, model10)
+stargazer(competent1, competent2, honest1, honest2, concern1, concern2,
+          reliable1, reliable2, values1, values2, type="text",star.cutoffs = c(0.05, 0.01, 0.001))
 
 #overalltrust model
 mturk$trust <- (mturk$dv1*0.20 + mturk$dv2*0.20 + mturk$dv3*0.20 + mturk$dv4*0.20 + mturk$dv5*0.20)
@@ -127,6 +109,8 @@ mturk$trust <- (mturk$dv1*0.20 + mturk$dv2*0.20 + mturk$dv3*0.20 + mturk$dv4*0.2
 trustlm <- lm(trust~candidate_race*attack + pid7 + female+ HS+ RR, data=mturk)
 trustlm2 <- lm(trust~candidate_race+attack + pid7 + female+ HS+ RR, data=mturk)
 stargazer(trustlm, trustlm2, type="text", star.cutoffs = c(0.05, 0.01, 0.001))
+
+t.test(trust~candidate_race, data=subset(mturk, congruent=="incongruent"))
 
 #graph
 
@@ -145,6 +129,6 @@ plot_model(trustlm, type = "pred", terms = c("attack", "candidate_race"),
     axis.text.x = element_text(size = 22),
     text = element_text(size = 25),
     legend.position = "bottom",
-    aspect.ratio = 1
+    aspect.ratio = 0.85
   ) + aes(shape = group)
   
